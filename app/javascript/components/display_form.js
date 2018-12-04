9e496a6054e552940f102e3ff1e99d74504920cc
François HAMEAU <f.hameau@gmail.com>
2018-12-04 16:58:49 +0100

    o

---
+++ app/javascript/components/display_form.js
@@ -0,0 +1,20 @@
+
+const displayForm = () => {
+  const buy = document.getElementById('buy');
+  const search = document.getElementById('search');
+  const form_search = document.querySelector('.search');
+  const form_buy = document.querySelector('.buy');
+
+  buy.addEventListener('click', () => {
+    form_buy.classList.remove('disabled');
+    form_search.classList.add('disabled');
+  });
+
+  search.addEventListener('click', () => {
+    form_search.classList.remove('disabled');
+    form_buy.classList.add('disabled');
+  });
+};
+
+
+export { displayForm }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import {displayForm} from "../components/display_form";
+
+if (document.getElementById('search')) {
+  displayForm()
+}
