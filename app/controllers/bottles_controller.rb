462f8b9142204d2b6ee3e6207542c02d50e95762
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-12-05 18:18:55 +0100

    resolve heroku conflicts

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,13 +1,10 @@
 class BottlesController < ApplicationController
   before_action :set_bottle, only: [:show]
   def show
-    respond_to |format|
-      format.html
-      format.js
-    end
   end
 
   private
+
   def bottle_params
     params.recquire(:bottle).permit(:title)
   end
