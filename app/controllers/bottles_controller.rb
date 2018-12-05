f71155b1aec2a396ef3fc8f9b5f433989f1f8bac
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-12-05 17:45:46 +0100

    modals for bottles and reviews

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,11 +1,16 @@
 class BottlesController < ApplicationController
   before_action :set_bottle, only: [:show]
   def show
+    respond_to |format|
+      format.html
+      format.js
+    end
   end
 
   private
   def bottle_params
-  params.recquire(:bottle).permit(:title)
+    params.recquire(:bottle).permit(:title)
+  end
 
   def set_bottle
     @bottle = Bottle.find(params[:id])
--- app/controllers/user_bottles_controller.rb
+++ app/controllers/user_bottles_controller.rb
@@ -2,7 +2,6 @@ class UserBottlesController < ApplicationController
 
   def index
     @user_bottles = current_user.user_bottles
-raise
   end
 
   def new
