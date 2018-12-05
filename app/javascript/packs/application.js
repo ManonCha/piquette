d7e55539eeb6d8fff2488e819585739909d89def
François HAMEAU <f.hameau@gmail.com>
2018-12-05 10:36:44 +0100

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
 import "bootstrap";
-import { initNavbar } from "../navbar"
+import { initNavbar } from "../components/navbar"
 
 import {displayForm} from "../components/display_form";
 import {hideForm} from "../components/hide_form";
 
 if (document.getElementById('search')) {
   displayForm()
-}
   hideForm()
+}
 
 initNavbar();
