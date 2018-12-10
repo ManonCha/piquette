1f8d9d3e9f682c19b6b6568cac088de0a861be22
François HAMEAU <f.hameau@gmail.com>
2018-12-10 14:46:30 +0100

    slide bar on reco

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,19 +3,19 @@ import "bootstrap";
 import { initNavbar } from "../components/navbar"
 import {displayForm} from "../components/display_form";
 import {hideForm} from "../components/hide_form";
+import {slide} from "../components/slide";
 import "../plugins/flatpickr";
 import "../plugins/aos"
 
 if (document.getElementById('search')) {
   displayForm()
-}
+};
 
+slide();
 
 initNavbar();
 
-
-
-// hideForm();
+// slide
 
 
 
