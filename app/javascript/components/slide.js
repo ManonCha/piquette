2fa500f6c40e7af221dadba2524dc30c5417a786
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-12-12 19:48:30 +0100

    reco form

--- app/javascript/components/slide.js
+++ app/javascript/components/slide.js
@@ -1,11 +1,14 @@
 const slide = () => {
   const slide = document.getElementById('price');
   const target = document.getElementById('target');
-
-  slide.addEventListener('change', (event) => {
-    // console.log(event.target.value);
-    target.innerText = event.target.value;
-  });
+  console.log(slide.value);
+  if (slide) {
+    target.innerText = slide.value;
+    console.log(target);
+    slide.oninput = function() {
+        target.innerText = this.value;
+    };
+  };
 };
 
 export { slide }
